159a1b9fdd24ac775326e7cb2d094bdf4a2303e7
worldeddt <ktest92@gmail.com>
2025-01-21 11:32:12 +0900

    refactor: crud test mock data 리팩토링

--- src/test/java/api/announcement/services/NoticeServiceTest.java
+++ src/test/java/api/announcement/services/NoticeServiceTest.java
@@ -12,8 +12,8 @@ import api.announcement.enums.NoticeStatus;
 import api.announcement.enums.Role;
 import api.announcement.repositories.NoticeRepository;
 import api.announcement.repositories.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -54,52 +54,81 @@ class NoticeServiceTest {
     private static final String NOTICE_CACHE_PREFIX = "notice:";
 
     String datetimeFormat = "yyyy-MM-dd HH:mm:ss";
+    Long noticeId;
+    Long attachmentId;
+    Long userId;
+    User user;
+    Notice notice;
+    Attachment attachment;
+    NoticeUpdateRequestDto noticeUpdateRequestDto;
+    NoticeRequestDto noticeRequestDto;
+    AttachmentRequestDto attachmentRequestDto;
 
+    @BeforeEach
+    public void init() {
+        //Mock data
+        noticeId = 1L;
+        attachmentId = 1L;
+        userId = 2L;
 
-    @Test
-    void createNotice() {
-
-        //mock data
-        User user = new User();
+        user = new User();
+        user.setId(userId);
         user.setRole(Role.ADMIN);
-        user.setId(1L);
-        user.setEmail("example@example.com");
-        user.setUsername("eddy");
-        user.setPassword(passwordEncoder.encode("user123"));
+        user.setUsername("admin");
+        user.setEmail("user@example.com");
+        user.setUsername(passwordEncoder.encode("eddy"));
 
-        AttachmentRequestDto attachmentRequestDto = AttachmentRequestDto.builder()
+        attachment = new Attachment();
+        attachment.setId(attachmentId);
+        attachment.setFileUrl("test.jpg");
+        attachment.setFileName("test.jpg");
+        attachment.setStatus(AttachmentStatus.ACTIVE);
+
+        notice = new Notice();
+        notice.setId(noticeId);
+        notice.setTitle("test");
+        notice.setContent("test contents");
+        notice.setStartDate(LocalDateTime.now().minusDays(1));
+        notice.setEndDate(LocalDateTime.now().plusDays(1));
+        notice.setCreatedUser(
+                user
+        );
+        notice.setAttachments(
+                List.of(
+                        attachment
+                )
+        );
+        notice.setStatus(NoticeStatus.ACTIVE);
+
+        noticeUpdateRequestDto =
+                NoticeUpdateRequestDto.builder()
+                        .title("update test")
+                        .content("update test contents")
+                        .startDate(LocalDateTime.now().minusDays(1))
+                        .endDate(LocalDateTime.now().plusDays(1))
+                        .updateUserId(userId)
+                        .viewed(true)
+                        .build();
+
+        attachmentRequestDto = AttachmentRequestDto.builder()
                 .fileName("test.jpg")
                 .filePath("/test.jpg")
                 .build();
 
-        NoticeRequestDto noticeRequestDto = NoticeRequestDto.builder()
-                .createId(user.getId())
-                .title("test")
-                .content("test contents")
-                .attachments(List.of(attachmentRequestDto))
-                .endDate(LocalDateTime.now().plusDays(1))
-                .startDate(LocalDateTime.now().minusDays(1))
-                .build();
+        noticeRequestDto =
+                NoticeRequestDto.builder()
+                        .createId(user.getId())
+                        .title("test")
+                        .content("test contents")
+                        .attachments(List.of(attachmentRequestDto))
+                        .endDate(LocalDateTime.now().plusDays(1))
+                        .startDate(LocalDateTime.now().minusDays(1))
+                        .build();
+    }
 
-        Notice notice = new Notice();
-        notice.setTitle(noticeRequestDto.getTitle());
-        notice.setContent(noticeRequestDto.getContent());
-        notice.setStartDate(noticeRequestDto.getStartDate());
-        notice.setEndDate(noticeRequestDto.getEndDate());
-        notice.setCreatedUser(user);
-        notice.setAttachments(
-                noticeRequestDto.getAttachments().stream().map(
-                        attachmentRequestDto1 -> {
-                            Attachment attachment = new Attachment();
-                            attachment.setFileUrl(attachmentRequestDto1.getFilePath());
-                            attachment.setFileName(attachmentRequestDto1.getFileName());
-                            attachment.setStatus(AttachmentStatus.ACTIVE);
-                            return attachment;
-                        }
-                ).toList()
-        );
-        notice.setStatus(NoticeStatus.ACTIVE);
 
+    @Test
+    void createNotice() {
         // Mock Repository
         when(userRepository.findById(user.getId()))
                 .thenReturn(Optional.of(user));
@@ -116,43 +145,12 @@ class NoticeServiceTest {
         assertEquals(noticeRequestDto.getAttachments().size(), responseDto.getAttachments().size());
 
         // Mock 동작 검증
-        verify(userRepository, times(1)).findById(1L);
+        verify(userRepository, times(1)).findById(user.getId());
         verify(noticeRepository, times(1)).save(any(Notice.class));
     }
 
     @Test
     void getNoticeById() {
-
-        //Given
-        Long noticeId = 1L;
-
-        User user = new User();
-        user.setId(1L);
-        user.setRole(Role.ADMIN);
-        user.setUsername("admin");
-        user.setEmail("user@example.com");
-        user.setUsername(passwordEncoder.encode("eddy"));
-
-        Attachment attachment = new Attachment();
-        attachment.setFileUrl("test.jpg");
-        attachment.setFileName("test.jpg");
-        attachment.setStatus(AttachmentStatus.ACTIVE);
-
-        Notice notice = new Notice();
-        notice.setId(noticeId);
-        notice.setTitle("test");
-        notice.setContent("test contents");
-        notice.setStartDate(LocalDateTime.now().minusDays(1));
-        notice.setEndDate(LocalDateTime.now().plusDays(1));
-        notice.setCreatedUser(
-            user
-        );
-        notice.setAttachments(
-                List.of(
-                        attachment
-                )
-        );
-
         //Mock Repository
         when(noticeRepository.findById(noticeId)).thenReturn(Optional.of(notice));
         when(redisService.hasKey(NOTICE_CACHE_PREFIX + noticeId)).thenReturn(true);
@@ -175,39 +173,6 @@ class NoticeServiceTest {
 
     @Test
     void deleteNotice() {
-        Long noticeId = 1L;
-        Long attachmentId = 1L;
-        Long userId = 2L;
-
-        User user = new User();
-        user.setId(userId);
-        user.setRole(Role.ADMIN);
-        user.setUsername("admin");
-        user.setEmail("user@example.com");
-        user.setUsername(passwordEncoder.encode("eddy"));
-
-        Attachment attachment = new Attachment();
-        attachment.setId(attachmentId);
-        attachment.setFileUrl("test.jpg");
-        attachment.setFileName("test.jpg");
-        attachment.setStatus(AttachmentStatus.ACTIVE);
-
-        Notice notice = new Notice();
-        notice.setId(noticeId);
-        notice.setTitle("test");
-        notice.setContent("test contents");
-        notice.setStartDate(LocalDateTime.now().minusDays(1));
-        notice.setEndDate(LocalDateTime.now().plusDays(1));
-        notice.setCreatedUser(
-                user
-        );
-        notice.setAttachments(
-                List.of(
-                        attachment
-                )
-        );
-        notice.setStatus(NoticeStatus.ACTIVE);
-
         //when
         when(noticeRepository.findById(noticeId)).thenReturn(Optional.of(notice));
         noticeService.deleteNotice(noticeId);
@@ -222,41 +187,6 @@ class NoticeServiceTest {
 
     @Test
     void getNotices() {
-
-        // Mock data
-        Long noticeId = 1L;
-        Long attachmentId = 1L;
-
-        User user = new User();
-        user.setId(1L);
-        user.setRole(Role.ADMIN);
-        user.setUsername("admin");
-        user.setEmail("user@example.com");
-        user.setUsername(passwordEncoder.encode("eddy"));
-
-        Attachment attachment = new Attachment();
-        attachment.setId(attachmentId);
-        attachment.setFileUrl("test.jpg");
-        attachment.setFileName("test.jpg");
-        attachment.setStatus(AttachmentStatus.ACTIVE);
-
-        Notice notice = new Notice();
-        notice.setId(noticeId);
-        notice.setTitle("test");
-        notice.setContent("test contents");
-        notice.setStartDate(LocalDateTime.now().minusDays(1));
-        notice.setEndDate(LocalDateTime.now().plusDays(1));
-        notice.setCreatedUser(
-                user
-        );
-        notice.setAttachments(
-                List.of(
-                        attachment
-                )
-        );
-        notice.setStatus(NoticeStatus.ACTIVE);
-
-
         Pageable pageable = PageRequest.of(0, 5);
 
         List<Notice> mockList = List.of(notice);
@@ -281,51 +211,6 @@ class NoticeServiceTest {
 
     @Test
     void updateNotice() {
-        //Mock data
-        Long noticeId = 1L;
-        Long attachmentId = 1L;
-        Long userId = 2L;
-
-        User user = new User();
-        user.setId(userId);
-        user.setRole(Role.ADMIN);
-        user.setUsername("admin");
-        user.setEmail("user@example.com");
-        user.setUsername(passwordEncoder.encode("eddy"));
-
-        Attachment attachment = new Attachment();
-        attachment.setId(attachmentId);
-        attachment.setFileUrl("test.jpg");
-        attachment.setFileName("test.jpg");
-        attachment.setStatus(AttachmentStatus.ACTIVE);
-
-        Notice notice = new Notice();
-        notice.setId(noticeId);
-        notice.setTitle("test");
-        notice.setContent("test contents");
-        notice.setStartDate(LocalDateTime.now().minusDays(1));
-        notice.setEndDate(LocalDateTime.now().plusDays(1));
-        notice.setCreatedUser(
-                user
-        );
-        notice.setAttachments(
-                List.of(
-                        attachment
-                )
-        );
-        notice.setStatus(NoticeStatus.ACTIVE);
-
-        NoticeUpdateRequestDto noticeUpdateRequestDto =
-                NoticeUpdateRequestDto.builder()
-                        .title("update test")
-                        .content("update test contents")
-                        .startDate(LocalDateTime.now().minusDays(1))
-                        .endDate(LocalDateTime.now().plusDays(1))
-                        .updateUserId(userId)
-                        .viewed(true)
-                        .build();
-
-
         //Mock Repository
         when(noticeRepository.findById(noticeId)).thenReturn(Optional.of(notice));
 
