3a4a5f3c53fabf40166a220cd9561791d6408165
worldeddt <ktest92@gmail.com>
2025-01-22 10:48:20 +0900

    feat: json format 추가 in noticerequestdto

--- src/main/java/api/announcement/controller/dto/NoticeRequestDto.java
+++ src/main/java/api/announcement/controller/dto/NoticeRequestDto.java
@@ -1,6 +1,7 @@
 package api.announcement.controller.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -19,9 +20,11 @@ public class NoticeRequestDto {
     private String content;
 
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDate;
 
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endDate;
 
     private List<AttachmentRequestDto> attachments;
